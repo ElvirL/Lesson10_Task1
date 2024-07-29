e2dffb86b6be283a1a4605dcb7a4532d4fed5231
elvir <hh.@mail.ru>
2024-07-29 23:38:53 +0500

    Task1

---
+++ main.cpp
@@ -0,0 +1,48 @@
+/*
+Вам представлена готовая реализация программы на языке C++, но она неисправна. Нужно исправить ошибки компиляции и исполнения.
+
+Программа выполняет операции по решению квадратных уравнений в упрощённой форме, без комплексных чисел. 
+Форма принимаемого квадратного уравнения:Требуется вычислить корни уравнения или корень, если он один.
+
+Дискриминант вычисляется по формуле: 
+
+Если дискриминант равен нулю, то корень один x = ?b/2a.
+
+Если больше нуля, то их два: 
+
+Исходный код программы, который требуется скопировать в редактор:
+*/
+
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+float a, b, c;
+std::cout << "a, b, c: ";
+std::cin >> a >> b >> c;
+if (a == 0)
+{
+std::cout << "Not a quadratic equation!" << std::endl;
+}
+else
+{
+float discriminant = b * b - 4 * a * c;
+
+if (discriminant > 1)
+{
+float x1 = (-b + std::sqrt(discriminant)) / (2 * a);
+float x2 = (-b - std::sqrt(discriminant)) / (2 * a);
+std::cout << "Root 1, 2: " << x1 << ", " << x2 << std::endl;
+}
+else if (discriminant == 0)
+{
+float x = -b / (2 * a);
+std::cout << "Root: " << x << std::endl;
+}
+else
+{
+std::cout << "Complex scenario is not supported!" << std::endl;
+}
+}
+}
